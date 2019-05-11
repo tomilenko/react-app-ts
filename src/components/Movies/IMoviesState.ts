efd943e50d1b977f0cfbbe5a5e969b80079d8993
Vadym Tomilenko <tomilenko.vadim@gmail.com>
2019-05-11 21:02:36 +0300

    Added Movies component

---
+++ src/components/Movies/IMoviesState.ts
@@ -0,0 +1,6 @@
+import { IMovie } from "../../definitions/IMovie";
+
+export interface IMoviesState {
+	items: [IMovie]
+}
+
---
+++ src/components/Movies/Movies.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { IMoviesState } from './IMoviesState';
+import { IMoviesProps } from './IMoviesProps';
+import MoviesService from '../../services/MoviesService';
+import MovieCard from '../MovieCard/MovieCard';
+import { tempMovie } from '../../definitions/tempMovie';
+import { IMovie } from "../../definitions/IMovie";
+
+class Movies extends React.Component<IMoviesProps, IMoviesState> {
+
+	constructor(props: IMoviesProps) {
+		super(props);
+		this.state = { items: [tempMovie] };
+	}
+
+	discover = () => {
+		let mService = new MoviesService();
+		mService.discoverMovies().then((data: [IMovie]) => {
+			console.warn(data);
+			this.setState({
+				items: data
+			});
+		});
+	}
+
+	componentWillMount = () => this.discover();
+
+	render = (): JSX.Element => {
+		return (
+			<div className="Movies">
+				<h2>Movies</h2>
+				<ul className="Movies-list">
+					{this.state.items.map( (item: IMovie) => (
+						<li className="Movies-list_item">
+							<MovieCard item={item} />
+						</li>
+					))}
+				</ul>
+			</div>
+		);
+	}
+}
+
+export default Movies;
