54708a40112802dcc82ff3ff399f4b130bbb12a4
Vadym Tomilenko <tomilenko.vadim@gmail.com>
2019-05-11 21:13:40 +0300

    Added movieDb config

--- src/services/MoviesService.ts
+++ src/services/MoviesService.ts
@@ -1,12 +1,12 @@
+import config from '../configs/config.json';
+
 const Urls: any = {
 	movies: 'discover/movie',
 	search: 'search?query='
   };
 
 const ServiceHelper: any = {
-	endpoint: "https://api.themoviedb.org/3/",
-	apiKey: "ec100598eff80eb95917837627303bb3",
-	makeUrl: (url: string) => `${ServiceHelper.endpoint}${url}?api_key=${ServiceHelper.apiKey}`,
+	makeUrl: (url: string) => `${config.movieDb.endpoint}${url}?api_key=${config.movieDb.apiKey}`,
 	validate: (res: any) => {
 		if (res.status >= 400) {
 		  throw new Error("Bad response from server");
