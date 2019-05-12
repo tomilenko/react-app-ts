58723607985d9bb8585f94f34f7a456ea2bf4cbb
Vadym Tomilenko <tomilenko.vadim@gmail.com>
2019-05-12 20:17:57 +0300

    Build production version

---
+++ build/precache-manifest.14829b1fd0d880368c97e012aa85017c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bfb13a92d22c86d691ee9b3e0764357",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a27caa5fdcd330623155",
+    "url": "/static/css/main.d0c4c1b3.chunk.css"
+  },
+  {
+    "revision": "cfd924f4db1d9e733e40",
+    "url": "/static/js/2.e8234ebf.chunk.js"
+  },
+  {
+    "revision": "a27caa5fdcd330623155",
+    "url": "/static/js/main.7dd9fa18.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
